291b333c49cb710c0be0373922953f5315612f86
Zer0dot <Zer0dot.dev@gmail.com>
2021-01-01 23:58:11 -0500

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,17 +16,17 @@ module.exports = {
     }
   },
   networks: {
-    mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyProjectId}`,
-      gasPrice: 45000000000,
-      gas: 3200000,
-      accounts: { mnemonic: ffmnemonic }
-    },
+    // mainnet: {
+    //   url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyProjectId}`,
+    //   gasPrice: 45000000000,
+    //   gas: 3200000,
+    //   accounts: { mnemonic: ffmnemonic }
+    // },
     hardhat: {
-      forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyProjectId}`,
-        blockNumber: 11449150
-      }
+      // forking: {
+      //   url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyProjectId}`,
+      //   blockNumber: 11449150
+      // }
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${infuraProjectId}`,
